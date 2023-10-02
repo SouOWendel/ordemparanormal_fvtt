3b96388012ac17d4ba65e553a1334384af6d350c
Mark57449 <souowendel@gmail.com>
2023-10-02 18:00:23 -0300

    style(eslint): lint the indentation

--- module/sheets/item-sheet.mjs
+++ module/sheets/item-sheet.mjs
@@ -4,60 +4,60 @@
  */
 export class OrdemItemSheet extends ItemSheet {
 
-  /** @override */
-  static get defaultOptions() {
-    return mergeObject(super.defaultOptions, {
-      classes: ["ordemparanormal_fvtt", "sheet", "item"],
-      width: 520,
-      height: 480,
-      tabs: [{ navSelector: ".sheet-tabs", contentSelector: ".sheet-body", initial: "description" }]
-    });
-  }
+	/** @override */
+	static get defaultOptions() {
+		return mergeObject(super.defaultOptions, {
+			classes: ['ordemparanormal_fvtt', 'sheet', 'item'],
+			width: 520,
+			height: 480,
+			tabs: [{ navSelector: '.sheet-tabs', contentSelector: '.sheet-body', initial: 'description' }]
+		});
+	}
 
-  /** @override */
-  get template() {
-    const path = "systems/ordemparanormal_fvtt/templates/item";
-    // Return a single sheet for all item types.
-    // return `${path}/item-sheet.html`;
+	/** @override */
+	get template() {
+		const path = 'systems/ordemparanormal_fvtt/templates/item';
+		// Return a single sheet for all item types.
+		// return `${path}/item-sheet.html`;
 
-    // Alternatively, you could use the following return statement to do a
-    // unique item sheet by type, like `weapon-sheet.html`.
-    return `${path}/item-${this.item.data.type}-sheet.html`;
-  }
+		// Alternatively, you could use the following return statement to do a
+		// unique item sheet by type, like `weapon-sheet.html`.
+		return `${path}/item-${this.item.data.type}-sheet.html`;
+	}
 
-  /* -------------------------------------------- */
+	/* -------------------------------------------- */
 
-  /** @override */
-  getData() {
-    // Retrieve base data structure.
-    const context = super.getData();
+	/** @override */
+	getData() {
+		// Retrieve base data structure.
+		const context = super.getData();
 
-    // Use a safe clone of the item data for further operations.
-    const itemData = context.item.data;
+		// Use a safe clone of the item data for further operations.
+		const itemData = context.item.data;
 
-    // Retrieve the roll data for TinyMCE editors.
-    context.rollData = {};
-    let actor = this.object?.parent ?? null;
-    if (actor) {
-      context.rollData = actor.getRollData();
-    }
+		// Retrieve the roll data for TinyMCE editors.
+		context.rollData = {};
+		const actor = this.object?.parent ?? null;
+		if (actor) {
+			context.rollData = actor.getRollData();
+		}
 
-    // Add the actor's data to context.data for easier access, as well as flags.
-    context.data = itemData.data;
-    context.flags = itemData.flags;
+		// Add the actor's data to context.data for easier access, as well as flags.
+		context.data = itemData.data;
+		context.flags = itemData.flags;
 
-    return context;
-  }
+		return context;
+	}
 
-  /* -------------------------------------------- */
+	/* -------------------------------------------- */
 
-  /** @override */
-  activateListeners(html) {
-    super.activateListeners(html);
+	/** @override */
+	activateListeners(html) {
+		super.activateListeners(html);
 
-    // Everything below here is only needed if the sheet is editable
-    if (!this.isEditable) return;
+		// Everything below here is only needed if the sheet is editable
+		if (!this.isEditable) return;
 
-    // Roll handlers, click handlers, etc. would go here.
-  }
+		// Roll handlers, click handlers, etc. would go here.
+	}
 }
