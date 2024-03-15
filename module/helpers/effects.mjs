427e1457e461106c1f4998d8efb4990734f4edb3
Wendel Henrique (SouOWendel) <souowendel@gmail.com>
2024-03-14 22:55:27 -0300

    style(effects helper): was changed the break line bracket style and indentation

--- module/helpers/effects.mjs
+++ module/helpers/effects.mjs
@@ -1,3 +1,4 @@
+/* eslint-disable indent */
 /**
  * Manage Active Effect instances through the Actor Sheet via effect control buttons.
  * @param {MouseEvent} event      The left-click event on the effect control
@@ -11,21 +12,23 @@ export function onManageActiveEffect(event, owner) {
 		? owner.effects.get(li.dataset.effectId)
 		: null;
 	switch (a.dataset.action) {
-	case 'create':
-		return owner.createEmbeddedDocuments('ActiveEffect', [
-			{
-				label: game.i18n.localize('ordemparanormal.newEffect'),
-				icon: 'icons/svg/aura.svg',
-				origin: owner.uuid,
-				'duration.rounds': li.dataset.effectType === 'temporary' ? 1 : undefined,
-				disabled: li.dataset.effectType === 'inactive',
-			}]);
-	case 'edit':
-		return effect.sheet.render(true);
-	case 'delete':
-		return effect.delete();
-	case 'toggle':
-		return effect.update({ disabled: !effect.disabled });
+		case 'create':
+			return owner.createEmbeddedDocuments('ActiveEffect', [
+				{
+					label: game.i18n.localize('ordemparanormal.newEffect'),
+					icon: 'icons/svg/aura.svg',
+					origin: owner.uuid,
+					'duration.rounds':
+						li.dataset.effectType === 'temporary' ? 1 : undefined,
+					disabled: li.dataset.effectType === 'inactive',
+				},
+			]);
+		case 'edit':
+			return effect.sheet.render(true);
+		case 'delete':
+			return effect.delete();
+		case 'toggle':
+			return effect.update({ disabled: !effect.disabled });
 	}
 }
 
