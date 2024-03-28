5f2fe88d1dcff2d48af85e06f844ad387cfd2a0a
Wendel Henrique (SouOWendel) <souowendel@gmail.com>
2024-03-27 22:05:22 -0300

    fix(macro): adjust the `getMacroTarget` to catch `allAplicableEffects` instead of `actor.effects`

--- module/documents/macro.mjs
+++ module/documents/macro.mjs
@@ -1,3 +1,4 @@
+/* eslint-disable indent */
 /**
  * Attempt to create a macro from the dropped data. Will use an existing macro if one exists.
  * @param {object} dropData     The dropped data
@@ -5,37 +6,38 @@
  */
 export async function createOPMacro(dropData, slot) {
 	const macroData = { type: 'script', scope: 'actor' };
-	switch ( dropData.type ) {
-	case 'Item': {
-		const itemData = await Item.implementation.fromDropData(dropData);
-		if ( !itemData ) return ui.notifications.warn(game.i18n.localize('MACRO.opUnownedWarn'));
-		foundry.utils.mergeObject(macroData, {
-			name: itemData.name,
-			img: itemData.img,
-			command: `ordemparanormal.documents.macro.rollItem('${itemData.name}')`,
-			flags: {'dnd5e.itemMacro': true}
-		});
-		break;
+	switch (dropData.type) {
+		case 'Item': {
+			const itemData = await Item.implementation.fromDropData(dropData);
+			if (!itemData) return ui.notifications.warn(game.i18n.localize('MACRO.opUnownedWarn'));
+			foundry.utils.mergeObject(macroData, {
+				name: itemData.name,
+				img: itemData.img,
+				command: `ordemparanormal.documents.macro.rollItem('${itemData.name}')`,
+				flags: { 'ordemparanormal.itemMacro': true },
+			});
+			break;
+		}
+		case 'ActiveEffect': {
+			const effectData = await ActiveEffect.implementation.fromDropData(dropData);
+			if (!effectData) return ui.notifications.warn(game.i18n.localize('MACRO.opUnownedWarn'));
+			foundry.utils.mergeObject(macroData, {
+				name: effectData.name,
+				img: effectData.icon,
+				command: `ordemparanormal.documents.macro.toggleEffect('${effectData.name}')`,
+				flags: { 'ordemparanormal.effectMacro': true },
+			});
+			break;
+		}
+		default:
+			return;
 	}
-	case 'ActiveEffect': {
-		const effectData = await ActiveEffect.implementation.fromDropData(dropData);
-		if ( !effectData ) return ui.notifications.warn(game.i18n.localize('MACRO.opUnownedWarn'));
-		foundry.utils.mergeObject(macroData, {
-			name: effectData.label,
-			img: effectData.icon,
-			command: `ordemparanormal.documents.macro.toggleEffect('${effectData.label}')`,
-			flags: {'dnd5e.effectMacro': true}
-		});
-		break;
-	}
-	default:
-		return;
-	}
-  
+
 	// Assign the macro to the hotbar
-	const macro = game.macros.find(m => {
-		return (m.name === macroData.name) && (m.command === macroData.command) && m.isAuthor;
-	}) || await Macro.create(macroData);
+	const macro =
+		game.macros.find((m) => {
+			return m.name === macroData.name && m.command === macroData.command && m.isAuthor;
+		}) || (await Macro.create(macroData));
 	game.user.assignHotbarMacro(macro, slot);
 }
 
@@ -50,23 +52,23 @@ export async function createOPMacro(dropData, slot) {
 function getMacroTarget(name, documentType) {
 	let actor;
 	const speaker = ChatMessage.getSpeaker();
-	if ( speaker.token ) actor = game.actors.tokens[speaker.token];
+	if (speaker.token) actor = game.actors.tokens[speaker.token];
 	actor ??= game.actors.get(speaker.actor);
-	if ( !actor ) return ui.notifications.warn(game.i18n.localize('MACRO.opNoActorSelected'));
-  
-	const collection = (documentType === 'Item') ? actor.items : actor.effects;
-	const nameKeyPath = (documentType === 'Item') ? 'name' : 'label';
-  
+	if (!actor) return ui.notifications.warn(game.i18n.localize('MACRO.opNoActorSelected'));
+
+	const collection = documentType === 'Item' ? actor.items : Array.from(actor.allApplicableEffects());
+
 	// Find item in collection
-	const documents = collection.filter(i => foundry.utils.getProperty(i, nameKeyPath) === name);
+	const documents = collection.filter((i) => i.name === name);
 	const type = game.i18n.localize(`DOCUMENT.${documentType}`);
-	if ( documents.length === 0 ) {
-	  return ui.notifications.warn(game.i18n.format('MACRO.opMissingTargetWarn', { actor: actor.name, type, name }));
+	if (documents.length === 0) {
+		ui.notifications.warn(game.i18n.format('MACRO.opMissingTargetWarn', { actor: actor.name, type, name }));
+		return null;
 	}
-	if ( documents.length > 1 ) {
-	  ui.notifications.warn(game.i18n.format('MACRO.opMultipleTargetsWarn', { actor: actor.name, type, name }));
+	if (documents.length > 1) {
+		ui.notifications.warn(game.i18n.format('MACRO.opMultipleTargetsWarn', { actor: actor.name, type, name }));
 	}
-    
+
 	return documents[0];
 }
 
@@ -80,9 +82,9 @@ function getMacroTarget(name, documentType) {
 export function rollItem(itemName) {
 	return getMacroTarget(itemName, 'Item').use();
 }
-  
+
 /* -------------------------------------------- */
-  
+
 /**
  * Toggle an effect on and off when a macro is clicked.
  * @param {string} effectName        Name of the effect to be toggled.
@@ -90,5 +92,5 @@ export function rollItem(itemName) {
  */
 export function toggleEffect(effectName) {
 	const effect = getMacroTarget(effectName, 'ActiveEffect');
-	return effect?.update({disabled: !effect.disabled});
+	return effect?.update({ disabled: !effect.disabled });
 }
