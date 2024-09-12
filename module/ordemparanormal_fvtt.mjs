5ed1f0b790a70b020debae94c1b86fe750e783fd
Wendel Henrique (SouOWendel) <souowendel@gmail.com>
2024-09-12 17:44:30 -0300

    chore: changed the twitter icon and url to bluesky icon and url

--- module/ordemparanormal_fvtt.mjs
+++ module/ordemparanormal_fvtt.mjs
@@ -383,9 +383,9 @@ Hooks.on('renderSettings', async (app, [html]) => {
 		<strong>${game.system.version}</strong> </span>
 		<p><span class="system-info" data-tooltip="${game.i18n.localize('ordemparanormal.sidebar.discord')}">
 		<i class="fa-brands fa-discord"></i> souowendel</span>&nbsp;&nbsp;
-		<a href="https://twitter.com/EuSouOWendel" target="_blank" 
-		data-tooltip="${game.i18n.localize('ordemparanormal.sidebar.twitter')}">
-		<span class="system-info"><i class="fa-brands fa-twitter"></i> eusouowendel</span></p>
+		<a href="https://bsky.app/profile/souowendel.bsky.social" target="_blank" 
+		data-tooltip="${game.i18n.localize('ordemparanormal.sidebar.bluesky')}">
+		<span class="system-info"><i class="fa-brands fa-bluesky"></i> souowendel</span></p>
   `;
 	if (pip) badge.querySelector('.system-info').insertAdjacentElement('beforeend', pip);
 	heading.insertAdjacentElement('afterend', badge);
