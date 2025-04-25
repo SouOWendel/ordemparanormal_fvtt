29bb6f8468e91fda76cf5bc116a8a6071be92427
SouOWendel <souowendel@gmail.com>
2025-04-24 23:17:02 -0300

    feat(d20roll): created a function to configure the d20 rolls
    this function will make the handle of informations about the roll, for example, mode, bonuses and
    formula, and launch the roll on chat or open the dialog for additional configuration.

---
+++ module/dice/dice.mjs
@@ -0,0 +1,55 @@
+/**
+ * 
+ * @param {*} param0 
+ * @returns 
+ */
+export async function d20Roll({
+	parts=[], data={}, event, critical=15, failure=3,
+	shiftFastForward, template, title,
+	chatMessage=true, messageData={}, rollMode, flavor, hasCritical=false, chatTemplate
+}={}) {
+
+	// Handle input arguments
+	let attrValue = data.attr[1];
+	const mode = (attrValue > 0) ? 'kh': 'kl';
+	if (mode == 'kl') attrValue = 2 + (attrValue*-1);
+	let formula = [`${attrValue}d20${mode}`];
+
+	// Additional numbers
+	parts = parts.filter((x) => (x !== null && x !== 0));
+	if (parts) formula = formula.concat(parts).join(' + ');
+	const defaultRollMode = rollMode || game.settings.get('core', 'rollMode');
+
+	// Construct the D20Roll instance
+	const roll = new CONFIG.Dice.D20Roll(formula, data, {
+		flavor: flavor || title,
+		defaultRollMode,
+		rollMode,
+		critical,
+		failure,
+		hasCritical,
+		chatTemplate
+	});
+
+	// Variável com os dados do Dialog
+	if (shiftFastForward) {
+		const configured = await roll.configureDialog({
+			title,
+			defaultRollMode,
+			template
+		});
+		if (configured === null) return null;
+	} else roll.options.rollMode ??= defaultRollMode;
+
+	// Evaluate the configured roll
+	await roll.evaluate({allowInteractive: (roll.options.rollMode ?? defaultRollMode) !== CONST.DICE_ROLL_MODES.BLIND});
+
+	// Attach original message ID to the message
+	messageData = foundry.utils.expandObject(messageData);
+	const messageId = event?.target.closest('[data-message-id]')?.dataset.messageId;
+	if ( messageId ) foundry.utils.setProperty(messageData, 'flags.shinobinosho.originatingMessage', messageId);
+
+	// Create a Chat Message
+	if ( roll && chatMessage ) await roll.toMessage(messageData);
+	return roll;
+}
