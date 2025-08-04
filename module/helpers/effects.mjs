0289a8d188e5055d0c07abd61f3a3f21e5c5de9d
SouOWendel <souowendel@gmail.com>
2025-08-03 22:36:27 -0300

    feat(effects.mjs): removed onManageActiveEffect function and sort the effects with categories

--- module/helpers/effects.mjs
+++ module/helpers/effects.mjs
@@ -1,37 +1,3 @@
-/* eslint-disable indent */
-/**
- * Manage Active Effect instances through the Actor Sheet via effect control buttons.
- * @param {MouseEvent} event      The left-click event on the effect control
- * @param {Actor|Item} owner      The owning document which manages this effect
- */
-export function onManageActiveEffect(event, owner) {
-	event.preventDefault();
-	const a = event.currentTarget;
-	const li = a.closest('li');
-	const effect = li.dataset.effectId
-		? owner.effects.get(li.dataset.effectId)
-		: null;
-	switch (a.dataset.action) {
-		case 'create':
-			return owner.createEmbeddedDocuments('ActiveEffect', [
-				{
-					label: game.i18n.localize('op.newEffect'),
-					icon: 'icons/svg/aura.svg',
-					origin: owner.uuid,
-					'duration.rounds':
-						li.dataset.effectType === 'temporary' ? 1 : undefined,
-					disabled: li.dataset.effectType === 'inactive',
-				},
-			]);
-		case 'edit':
-			return effect.sheet.render(true);
-		case 'delete':
-			return effect.delete();
-		case 'toggle':
-			return effect.update({ disabled: !effect.disabled });
-	}
-}
-
 /**
  * Prepare the data structure for Active Effects which are currently applied to an Actor or Item.
  * @param {ActiveEffect[]} effects    The array of Active Effect instances to prepare sheet data for
@@ -62,6 +28,11 @@ export function prepareActiveEffectCategories(effects) {
 		if (e.disabled) categories.inactive.effects.push(e);
 		else if (e.isTemporary) categories.temporary.effects.push(e);
 		else categories.passive.effects.push(e);
+	}
+
+	// Sort each category
+	for (const c of Object.values(categories)) {
+		c.effects.sort((a, b) => (a.sort || 0) - (b.sort || 0));
 	}
 	return categories;
 }
