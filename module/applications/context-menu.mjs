8e5c74f41ce406319059d2da142ea64bb102d276
SouOWendel <souowendel@gmail.com>
2026-01-31 16:16:51 -0300

    fix(context-menu): extend ContextMenu from the correct implementation class (v13+)

--- module/applications/context-menu.mjs
+++ module/applications/context-menu.mjs
@@ -2,7 +2,7 @@
  * A specialized subclass of ContextMenu that places the menu in a fixed position.
  * @extends {ContextMenu}
  */
-export default class ContextMenu5e extends ContextMenu {
+export default class ContextMenu5e extends foundry.applications.ux.ContextMenu.implementation {
 	/** @override */
 	_setPosition(html, target, options={}) {
 		html.classList.add('ordemparanormal');
