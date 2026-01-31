9bf545a5a42bffeeaba817c870b69b85bf41f27f
SouOWendel <souowendel@gmail.com>
2026-01-31 17:57:54 -0300

    fix(item-sheet): update DragDrop implementation to use the correct reference

--- module/sheets/item-sheet.mjs
+++ module/sheets/item-sheet.mjs
@@ -577,7 +577,7 @@ export class OrdemItemSheet extends api.HandlebarsApplicationMixin(sheets.ItemSh
 				dragover: this._onDragOver.bind(this),
 				drop: this._onDrop.bind(this),
 			};
-			return new DragDrop(d);
+			return new foundry.applications.ux.DragDrop.implementation(d);
 		});
 	}
 
